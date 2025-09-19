3b14ee31d65a335ce2ecf1b7ce0912891e5dd011
Naimur Rahman Niloy <niloyrahman432@gmail.com>
2025-09-19 20:38:29 +0600

    started working

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,8 @@ export default function Navbar() {
 
     return(
         <div>
-        <div className="navbar text-black shadow-sm p-2">
-  <div className="navbar-start">
+        <div className="navbar text-black shadow-sm p-2  rounded-full">
+  <div className="navbar-start m-8">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
