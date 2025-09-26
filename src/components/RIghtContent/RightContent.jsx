607bbd97754ae726d2d4b7625060bd63bcab7132
Naimur Rahman Niloy <niloyrahman432@gmail.com>
2025-09-26 21:42:07 +0600

    done customizing

--- src/components/RIghtContent/RightContent.jsx
+++ src/components/RIghtContent/RightContent.jsx
@@ -64,16 +64,13 @@ export default function RightContent() {
         </div>
       </div>
       <div>
-        <div className="card card-border bg-base-100 w-96">
+        <div className="card card-border mt-10 bg-white text-black w-96">
           <div className="card-body">
-            <h2 className="card-title">Card Title</h2>
+            <h2 className="card-title">Lab Result</h2>
             <p>
               A card component has a figure, a body part, and inside body there
               are title and actions parts
             </p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
           </div>
         </div>
       </div>
